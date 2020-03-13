757891ecb249f46be8c30d9e35bcec89b7e4cbc0
Newton Angelini <newton.beck@gmail.com>
2020-03-13 16:21:21 -0300

    Lê os dados para conexão do banco por variáveis de ambiente

--- src/main/java/br/com/caelum/forum/infra/ConnectionFactory.java
+++ src/main/java/br/com/caelum/forum/infra/ConnectionFactory.java
@@ -6,13 +6,23 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/my_blog?useSSL=false";
-    private static final String USER = "root";
-    private static final String PASSWORD = "toor";
+    private static final String DEFAULT_URL = "jdbc:mysql://localhost:3306/my_blog?useSSL=false";
+    private static final String DEFAULT_USER = "root";
+    private static final String DEFAULT_PASSWORD = "toor";
+
+    private String url;
+    private String user;
+    private String password;
+
+    public ConnectionFactory() {
+        this.url = System.getenv("MY_BLOG_DB_URL") != null ? System.getenv("MY_BLOG_DB_URL") : DEFAULT_URL;
+        this.user = System.getenv("MY_BLOG_DB_USER") != null ? System.getenv("MY_BLOG_DB_USER") : DEFAULT_USER;
+        this.password = System.getenv("MY_BLOG_DB_PASSWORD") != null ? System.getenv("MY_BLOG_DB_PASSWORD") : DEFAULT_PASSWORD;
+    }
 
     public Connection create() {
         try {
-            return DriverManager.getConnection(URL, USER, PASSWORD);
+            return DriverManager.getConnection(this.url, this.user, this.password);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
